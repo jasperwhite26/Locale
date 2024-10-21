e80253ec5014e22b233ba3050680bc7b8e345aee
jasper <whiteana001@warren.k12.in.us>
2024-10-21 13:37:01 -0400

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class App {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static java.lang.System.out;
+
+public class App {
+    public static void main(String[] args) {
+        Locale usLocale = Locale.US;
+        Locale franceLocale = Locale.FRANCE;
+        Locale japanLocale = Locale.JAPAN;
+
+        LocalDate now = LocalDate.now();
+
+        printFormattedDate(usLocale, now);
+        printFormattedDate(franceLocale, now);
+        printFormattedDate(japanLocale, now);
+
+    }
+
+    private static void printFormattedDate(Locale locale, LocalDate date) {
+        out.println(locale.getLanguage());
+        out.println(locale.getCountry());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM/d/yyyy", locale);
+        out.println(formatter);
+        DateTimeFormatter full = DateTimeFormatter.ofPattern("EEEE, MMMM, d, yyyy", locale);
+        out.println(full);
+        out.println();
+    }
 }
